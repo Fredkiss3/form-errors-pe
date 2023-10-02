2f6debb35c5de8c73b580bb670882c5c0277e8de
Adrien KISSIE <fredkiss3@gmail.com>
2023-10-03 00:39:05 +0200

    🐛 bug repro

--- src/middleware.ts
+++
@@ -1,6 +0,0 @@
-import { NextRequest, NextResponse } from "next/server";
-
-export default function middleware(request: NextRequest) {
-    console.log("middleware");
-    return NextResponse.next();
-}
